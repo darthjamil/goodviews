c3cb130c28f2cebfbc2476455fdd3af87bc719b3
Nick Jamil <nickjamil@hotmail.com>
2020-04-29 16:35:38 -0400

    Add security scanning package

--- src/Goodviews.App/Controllers/WeatherForecastController.cs
+++ src/Goodviews.App/Controllers/WeatherForecastController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -26,12 +26,11 @@ namespace Goodviews.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = RandomNumberGenerator.GetInt32(-20, 55),
+                Summary = Summaries[RandomNumberGenerator.GetInt32(0, Summaries.Length)]
             })
             .ToArray();
         }
